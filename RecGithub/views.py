82927ed64e753467197ba65b194468b0c5d67be7
ch710798472 <710798472@qq.com>
2016-02-24 13:19:18 +0800

    add search code

--- RecGithub/views.py
+++ RecGithub/views.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 from django.shortcuts import render
-
+from github import Github
+import json
 # Create your views here.
 from django.http import HttpResponse
 # 引入我们创建的表单类
@@ -19,7 +20,7 @@ def add2(request, a, b):
     return HttpResponse(str(c))
 def home(request):
     TutorialList = ["HTML", "CSS", "jQuery", "Python", "Django"]
-    return render(request, 'home.html', {'TutorialList': TutorialList})
+    return render(request, 'search.html', {'TutorialList': TutorialList})
 def home1(request):
     List = map(str, range(100))# 一个长度为100的 List
 
@@ -33,11 +34,14 @@ def form(request):
             location = form.cleaned_data['location']
             language = form.cleaned_data['language']
 
-            return HttpResponse(str(GetUser(location,language)))
+            if GetSearchInfo(location,language):
+                return render(request,'search_result.html')
+            else:
+                return HttpResponse(str("查找结果不存在，请重新输入！"))
 
     else:# 当正常访问时
         form = SearchForm()
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'search.html', {'form': form})
 
 def GetUser(location,language):
     d = {"items":[]}
@@ -50,3 +54,63 @@ def GetUser(location,language):
         temp = r.json()
         d['items'].extend(temp['items'])
     return d
+
+def GetSearchInfo(location,language):
+    #search 100 users from location,language
+    d = {"items":[]}
+    url = 'https://api.github.com/search/users?sort=followers&q=location:%s+language:%s&per_page=100&page=' % (location,language)
+    rank_count=1
+
+    #获取排名
+    for i in range(rank_count):
+        print url + str(i+1)
+        newUrl = url + str(i+1)
+        r = requests.get(newUrl)
+        temp = r.json()
+        d['items'].extend(temp['items'])
+
+    print 'GetSearchInfo->finish rank user'
+
+    #获取用户详细信息
+    username = 'ch710798472'
+    password = 'Mm456123'
+    ACCESS_TOKEN = '0d1c6d6da836bc28b691f87dd34a1fbdc604c895'
+    client = Github(username,password=password, per_page=100)
+    t = {}
+    notdone=[]
+    j=0
+    for i in d['items']:
+        try:
+            user = client.get_user(i['login'])
+            t[user._rawData['login']] = user._rawData
+        except Exception,e:
+            print "GetSearchInfo->time out"
+            notdone.append(i['login'])
+        if (j%10)==0:
+            print j
+        j = j + 1
+
+    print 'GetSearchInfo->finish user info'
+
+    #重新排序
+
+    d1 = []#最后的结果存储
+    for i in d['items']:
+        for k in t:
+            try:
+                if i['login'] == k:
+#                   print k
+                    d1.append(t[k])
+#                   apendx.append(k)
+            except Exception,e:
+                pass
+
+    print 'GetSearchInfo->DONE'
+
+    if len(d1):
+        # filename=+ location+ '_'+language+ '.json'
+        filename='../static/data/'+'search.json'
+        json.dump(d1, open(filename, 'w'))
+        return 1
+    else:
+        return 0
